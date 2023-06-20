6ca56b366b0e3292cc40238d806cff0bdc1c587d
unknown <nmanagi23@gmail.com>
2023-06-21 00:07:52 +0100

    upated commit

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const button = document.getElementById('button')
-const mobile_menu = document.querySelector('.mobile-menu')
+const button = document.getElementById("button");
+const mobile_menu = document.getElementById("mobile--menu");
+const close_button = document.getElementById("Close--button")
 
-button.addEventListener('click', ()=>{
-    mobile_menu.style.display = 'block'
-})
+button.addEventListener("click", () => {
+  mobile_menu.style.display = "block";
+  button.style.display = "none"
+
+});
+
+close_button.addEventListener("click", () => {
+  mobile_menu.style.display = "none";
+  button.style.display = "block"
+
+});
