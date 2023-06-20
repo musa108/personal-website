74c0c22855cd0c9ebb6ba40449421d9f87410f61
unknown <nmanagi23@gmail.com>
2023-06-20 23:33:33 +0100

    update commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('button')
+const mobile_menu = document.querySelector('.mobile-menu')
+
+button.addEventListener('click', ()=>{
+    mobile_menu.style.display = 'block'
+})
